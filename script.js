c6e43eb7a3175b8aacbf34f4d115d1a6bfcd6bee
BenRaymon <bendraymon@gmail.com>
2022-09-13 14:45:27 -0400

    adjusted grid to size to any browser, moved column elements inside row divs

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 // Game constants and variables
-let divSquaresInGrid = document.querySelectorAll('.grid div');
-let currentIndex = 76; // Because 76 is the position of the starting-block 
-const wideness = 9;
+let divRowsInGrid = document.querySelectorAll('.grid .row');
+let currentColumn = 6;
+let currentRow = 10;
 const gameOverSound = new Audio('assets/audio_dead.mp3');
 const moveSound = new Audio("assets/audio_move.mp3");
 
 
 function moveFrog(e){
     moveSound.play();
-    divSquaresInGrid[currentIndex].classList.remove('frog');
+    divRowsInGrid[currentRow-1].children[currentColumn-1].classList.remove('frog');
     switch(e.key){
         case 'ArrowLeft':
             console.log('Move left');
-            currentIndex -= 1;
+            currentColumn -= 1;
             break;
         case 'ArrowRight':
             console.log('Move right');
-            currentIndex += 1;
+            currentColumn += 1;
             break;
         case 'ArrowUp':
             console.log('Move Up');
-            currentIndex -= wideness;
+            currentRow -= 1;
             break;
         case 'ArrowDown':
             console.log('Move left');
-            currentIndex += wideness;
+            currentRow += 1;
             break;  
     }
-
-    divSquaresInGrid[currentIndex].classList.add('frog');
-
+    divRowsInGrid[currentRow-1].children[currentColumn-1].classList.add('frog');
 }
 
 document.addEventListener('keydown', moveFrog)
