aacf3375a48049e4712ba29be55229e68568002c
BenRaymon <bendraymon@gmail.com>
2022-09-18 21:24:05 -0400

    cleaned code - created moveObjects method, flip object image if moving left

--- script.js
+++ script.js
@@ -49,20 +49,20 @@ let board = [
     { 
         row: 1, 
         environment: 'river', 
-        objects: [{ column: 0, direction: 1, image: "url"},
-            { column: 5, direction: 1, image: "url"},] 
+        objects: [{ column: 0, direction: 1, type: 'log', image: "url"},
+            { column: 5, direction: 1, type: 'log', image: "url"},] 
     },
     { 
         row: 2, 
         environment: 'river', 
-        objects: [{ column: 0, direction: -1, image: "url"},
-        { column: 5, direction: -1, image: "url"},] 
+        objects: [{ column: 0, direction: -1, type: 'log',  image: "url"},
+        { column: 5, direction: -1, type: 'log',  image: "url"},] 
     },
     { 
         row: 3, 
         environment: 'river', 
-        objects: [{ column: 0, direction: 1, image: "url"},
-        { column: 5, direction: 1, image: "url"},]  
+        objects: [{ column: 0, direction: 1, type: 'log',  image: "url"},
+        { column: 5, direction: 1, type: 'log',  image: "url"},]  
     },
     { 
         row: 4, 
@@ -72,17 +72,17 @@ let board = [
     { 
         row: 5, 
         environment: 'road', 
-        objects: [] 
+        objects: [ { column: 3, direction: 1, type: 'car',  image: "url"}, { column: 7, direction: 1, type: 'car',  image: "url"}] 
     },
     { 
         row: 6, 
         environment: 'road', 
-        objects: [ { column: 6, direction: -1, image: "url"}, { column: 2, direction: -1, image: "url"}] 
+        objects: [ { column: 6, direction: -1, type: 'car',  image: "url"}, { column: 2, direction: -1, type: 'car',  image: "url"}] 
     },
     { 
         row: 7, 
         environment: 'road', 
-        objects: [{ column: 0, direction: 1, image: "url"}, { column: 4, direction: 1, image: "url"}] 
+        objects: [{ column: 0, direction: 1, type: 'car', image: "url"}, { column: 4, direction: 1, type: 'car', image: "url"}] 
     },
     { 
         row: 8, 
@@ -133,162 +133,73 @@ function moveFrog(e){
 
 document.addEventListener('keydown', moveFrog)
 
-/*
-This function adds the movement functionality to all the logs
-moving left by calling logLeftMovement() function on each of them.
-*/
-function addMovementToLogsMovingLeftRight(){
-    //logsMovingLeft.forEach(logMovingLeft => logLeftMovement(logMovingLeft));
-    //logsMovingRight.forEach(logMovingRight => logRightMovement(logMovingRight));
-}
-
-function addMovementToCarsMovingLeftRight(){
-    //moveObjects();
-    //carsMovingLeft.forEach(carMovingLeft => carLeftMovement(carMovingLeft));
-    //carsMovingRight.forEach(carMovingRight => carRightMovement(carMovingRight));
-}
-
-// function logLeftMovement(logMovingLeft){
-//     if (logMovingLeft.classList.contains('l1')){
-//         logMovingLeft.classList.remove('l1');
-//         logMovingLeft.classList.add('l2');
-//     }
-
-//     else if(logMovingLeft.classList.contains('l2')){
-//         logMovingLeft.classList.remove('l2');
-//         logMovingLeft.classList.add('l3');
-//     }
-
-//     else if(logMovingLeft.classList.contains('l3')){
-//         logMovingLeft.classList.remove('l3');
-//         logMovingLeft.classList.add('l4');
-//     }
-
-//     else if(logMovingLeft.classList.contains('l4')){
-//         logMovingLeft.classList.remove('l4');
-//         logMovingLeft.classList.add('l5');
-//     }
-
-//     else if(logMovingLeft.classList.contains('l5')){
-//         logMovingLeft.classList.remove('l5');
-//         logMovingLeft.classList.add('l1');
-//     }
-// }
-
-
-// function logRightMovement(logMovingRight){
-//     if (logMovingRight.classList.contains('l1')){
-//         logMovingRight.classList.remove('l1');
-//         logMovingRight.classList.add('l5');
-//     }
-
-//     else if(logMovingRight.classList.contains('l2')){
-//         logMovingRight.classList.remove('l2');
-//         logMovingRight.classList.add('l1');
-//     }
-
-//     else if(logMovingRight.classList.contains('l3')){
-//         logMovingRight.classList.remove('l3');
-//         logMovingRight.classList.add('l2');
-//     }
-
-//     else if(logMovingRight.classList.contains('l4')){
-//         logMovingRight.classList.remove('l4');
-//         logMovingRight.classList.add('l3');
-//     }
-
-//     else if(logMovingRight.classList.contains('l5')){
-//         logMovingRight.classList.remove('l5');
-//         logMovingRight.classList.add('l4');
-//     }
-// }
-
-// function carLeftMovement(carMovingLeft){
-//     if (carMovingLeft.classList.contains('c1')){
-//         carMovingLeft.classList.remove('c1');
-//         carMovingLeft.classList.add('c2');
-//     }
-
-//     else if(carMovingLeft.classList.contains('c2')){
-//         carMovingLeft.classList.remove('c2');
-//         carMovingLeft.classList.add('c3');
-//     }
-
-//     else if(carMovingLeft.classList.contains('c3')){
-//         carMovingLeft.classList.remove('c3');
-//         carMovingLeft.classList.add('c4');
-//     }
-
-//     else if(carMovingLeft.classList.contains('c4')){
-//         carMovingLeft.classList.remove('c4');
-//         carMovingLeft.classList.add('c1');
-//     }
-// }
-
-// function carRightMovement(carMovingRight){
-//     if (carMovingRight.classList.contains('c1')){
-//         carMovingRight.classList.remove('c1');
-//         carMovingRight.classList.add('c4');
-//     }
-
-//     else if(carMovingRight.classList.contains('c2')){
-//         carMovingRight.classList.remove('c2');
-//         carMovingRight.classList.add('c1');
-//     }
-
-//     else if(carMovingRight.classList.contains('c3')){
-//         carMovingRight.classList.remove('c3');
-//         carMovingRight.classList.add('c2');
-//     }
-
-//     else if(carMovingRight.classList.contains('c4')){
-//         carMovingRight.classList.remove('c4');
-//         carMovingRight.classList.add('c3');
-//     }
-// }
-
+// calls moveObjects with road to move all objects on the roads
 function moveCars(){
-    for (var i = 0; i < board.length; i++){
-        if(board[i].environment == 'road'){
-            if (board[i].objects.length >= 1){
-                for(var j = 0; j < board[i].objects.length; j++){
-                    // remove the car from the current square
-                    squares[board[i].row].children[(board[i].objects[j].column + COLUMNS ) % COLUMNS].classList.remove('car');
-                    board[i].objects[j].column += (1 * board[i].objects[j].direction + COLUMNS) % COLUMNS;
-                    // add the car to the next square
-                    squares[board[i].row].children[(board[i].objects[j].column + COLUMNS ) % COLUMNS].classList.add('car');
-                }
-            }
-        }
-    }
+    moveObjects('road');
 }
 
-
+// calls moveObjects with river to move all objects on the rivers
 function moveLogs(){
+    moveObjects('river');
+}
+
+// move all objects that are in the environment specified
+// this function works by removing the class of the object type (e.g. car, log)
+// increases the column number of the object (or decreases if moving left)
+// and adds the object type to the class list of the div with the newly advanced column number
+function moveObjects(environment){
     for (var i = 0; i < board.length; i++){
-        if(board[i].environment == 'river'){
+        if(board[i].environment == environment){
             if (board[i].objects.length >= 1){
-                for(var j = 0; j < board[i].objects.length; j++){
-                    // remove the log from the current square (log is 2 squares wide)
-                    squares[board[i].row].children[board[i].objects[j].column % COLUMNS].classList.remove('log');
-                    squares[board[i].row].children[(board[i].objects[j].column + 1) % COLUMNS].classList.remove('log');
-                    //squares[board[i].row].children[(board[i].objects[j].column + 2) % COLUMNS].classList.remove('log');
-                    board[i].objects[j].column += (1 * board[i].objects[j].direction + COLUMNS) % COLUMNS;
-                    // add log to next square (log is 2 squares wide)
-                    squares[board[i].row].children[(board[i].objects[j].column + COLUMNS ) % COLUMNS].classList.add('log');
-                    squares[board[i].row].children[(board[i].objects[j].column + COLUMNS + 1) % COLUMNS].classList.add('log');
-                    //squares[board[i].row].children[(board[i].objects[j].column + COLUMNS + 2) % COLUMNS].classList.add('log');
+
+                let objects = board[i].objects;
+                let rowDiv = squares[board[i].row];
+
+                for(var j = 0; j < objects.length; j++){
+                    // remove the object class (car or log) from the current square
+                    removeClass(rowDiv, objects[j].column, objects[j].type);
+                    // logs are 2 squares wide
+                    if (objects[j].type == 'log')
+                        removeClass(rowDiv, objects[j].column + 1, objects[j].type);
+                    // flip the image if moving left
+                    if(objects[j].direction < 0)
+                        removeClass(rowDiv, objects[j].column, 'left');
+                    
+                    // move objects by one grid square
+                    objects[j].column += (1 * objects[j].direction + COLUMNS) % COLUMNS;
+                    
+                    // remove the object class (car or log) from the current square
+                    addClass(rowDiv, objects[j].column, objects[j].type);
+                    // logs are 2 squares wide
+                    if (objects[j].type == 'log')
+                        addClass(rowDiv, objects[j].column + 1, objects[j].type);
+                    // flip the image if moving left
+                    if(objects[j].direction < 0)
+                        addClass(rowDiv, objects[j].column, 'left');
                 }
             }
         }
     }
 }
+
+// the following functions add/remove the className string
+// to the classList of a specified column in the given rowDiv
+// (columnNum + COLUMNS) % COLUMNS ensures the objects 
+// loop back around instead of disappearing off screen
+function addClass(rowDiv, columnNum, className){
+    rowDiv.children[(columnNum + COLUMNS) % COLUMNS].classList.add(className);
+}
+
+function removeClass(rowDiv, columnNum, className){
+    rowDiv.children[(columnNum + COLUMNS) % COLUMNS].classList.remove(className);
+}
+
 
 
 /*
 We want to keep the logs and cars moving continously, 
-to do that we use setInterval(), which calls addMovementToLogsMovingLeft()
-function every 1 second or 1000ms.
+to do that we use setInterval(), which calls moveLogs 
+function every 1 second or 1000ms and moveCars every 600ms.
 */
-setInterval(moveLogs, 1000)
-setInterval(moveCars, 600)
+setInterval(moveLogs, 1000);
+setInterval(moveCars, 600);
